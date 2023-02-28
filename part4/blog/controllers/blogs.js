e8e4d61051151c5a1a4450689b18e6458e687b14
Lauri Soivi <soivi.lauri@gmail.com>
2023-02-28 21:55:37 +0200

    blog: part 4.19: Users added
    Blogs have info who user created blog and users have info all of user's blogs
    
    Login:
    - User can login and gets token
    
    Users:
    - List users
    - Add users
    
    Blogs:
    - Add new blogs if user is logged in and token is valid

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,14 +1,43 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  if(!user) {
+    return response.status(401).json({ error: 'user is missing' })
+  }
+
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes,
+    user: user
+  })
+
   const result = await blog.save()
+  user.blogs = user.blogs.concat(result._id)
+  await user.save()
   response.status(201).json(result)
 })
 
--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -5,6 +5,12 @@ const unknownEndpoint = (request, response) => {
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name ===  'JsonWebTokenError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired'
+    })
   }
 
   next(error)
